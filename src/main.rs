aa7acedec0f7c709ec4a689755afbd8c3a18ab80
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-01-17 01:04:35 +0530

    TCP listener in tokio

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
 #![allow(non_snake_case)]
 use std::thread;
 use tokio::time::{sleep, Duration};
+use tokio::net::TcpListener;
 #[tokio::main]
 async fn main() {
-    let _x = sam().await;
+    // let _x = sam().await;
+    // we nee a TCP listener
+    let listener = TcpListener::bind("127.0.0.1:3000").await;
+    match listener{
+        Ok(val)=> {
+            println!("Its a success");
+            loop{
+                let (socker,addr) = val.accept().await.unwrap();
+                println!("The stream : {:?}", socker);
+                println!("The addr : {:?}", addr);
+            }
+
+
+        },
+        Err(err) => println!("Its a failure")
+    }
+
+
+
 
 }
 
