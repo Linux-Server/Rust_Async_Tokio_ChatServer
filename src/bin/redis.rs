330d7e7842f7dcae80e7f4f82c18ed92b91d857c
Sachin Murali <67502584+Linux-Server@users.noreply.github.com>
2024-01-18 19:05:03 +0530

    time added

---
+++ src/bin/redis.rs
@@ -0,0 +1,29 @@
+use mini_redis::{client, Result};
+use tokio::time::{Duration,sleep};
+#[tokio::main]
+async fn main()-> Result<()> {
+     println!("The starting...");
+     let mut client = client::connect("0.0.0.0:3000").await.unwrap();
+
+
+     // Set the key "hello" with value "world"
+     let x = client.set("bello", "world".into()).await;
+
+     match x{
+          Ok(val) => println!("The val is {:?}", val),
+          Err(err) => println!("the custom error is {:?}", err)
+     }
+
+     println!("The end...");
+
+
+     // Get key "hello"
+     let result = client.get("bello").await?;
+
+     println!("got value from the server; result={:?}", result);
+
+     Ok(())
+
+
+
+}
--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,7 +1,8 @@
 #![allow(non_snake_case)]
 use std::thread;
 use tokio::time::{sleep, Duration};
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
+use mini_redis::{Frame,Connection};
 #[tokio::main]
 async fn main() {
     // let _x = sam().await;
@@ -9,11 +10,16 @@ async fn main() {
     let listener = TcpListener::bind("0.0.0.0:3000").await;
     match listener{
         Ok(val)=> {
-            println!("Its a success");
+            println!("Listening on 3000");
             loop{
-                let (socker,addr) = val.accept().await.unwrap();
-                println!("The stream : {:?}", socker);
-                println!("The addr : {:?}", addr);
+                let (socket,addr) = val.accept().await.unwrap();
+
+                let response = tokio::spawn(async move{
+                    process(socket).await;
+                }).await;
+             
+            
+               
             }
 
 
@@ -27,12 +33,25 @@ async fn main() {
 }
 
 
-async fn sam()->i32{
-    println!("Before sleep..");
-    // thread::sleep(Duration::from_secs(5));
-    let _b = sleep(Duration::from_secs(5)).await;
-    println!("After sleep..");
-    10
+async fn process(socket:TcpStream){
+    let mut conn = Connection::new(socket);
+    let data = conn.read_frame().await;
+    match data{
+        Ok(val)=>{
+            match val{
+                Some(frame) => {
+                    println!("The frame received : {:?}", frame);
+                    let res = Frame::Error("Got the package...".to_owned());
+                    let x = conn.write_frame(&res).await;
+                   
+                },
+                None => println!("Not frame found")
+            }
+        },
+        Err(err) => {
+            println!("Error occured : {:?}",err)
+        }
+    }
 
 }
 
